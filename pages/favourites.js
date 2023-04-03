9a75a87be69ac0537a8890accb2a6d8189e41b3c
at0mofficial <sangwanshivam762001@gmail.com>
2023-04-02 22:41:54 -0400

    First Commit

---
+++ pages/favourites.js
@@ -0,0 +1,30 @@
+import { useAtom } from "jotai";
+import { favouritesAtom } from "@/store";
+import { Row, Col, Card } from "react-bootstrap";
+import ArtworkCard from "@/components/ArtworkCard";
+
+export default function Favourites(){
+    const [favouritesList] = useAtom(favouritesAtom);
+    if(!favouritesList) return null;
+    return(<>
+    <div>
+      {favouritesList.length > 0 ? (
+        <Row className="gy-4">
+          {favouritesList.map((currentObjectID) => (
+            <Col lg={3} key={currentObjectID}>
+              <ArtworkCard objectID={currentObjectID} />
+            </Col>
+          ))}
+        </Row>
+      ) : (
+        <Card>
+          <Card.Body>
+            <h4>Nothing Here</h4>
+            Try adding some new artwork to the list.
+          </Card.Body>
+        </Card>
+      )}
+    </div>
+    </>)
+
+}
